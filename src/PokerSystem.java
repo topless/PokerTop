99527443868f1c5d5324f4c74202553f28298b44
Chris Topaloudis <chris@chris-laptop.(none)>
2010-05-02 23:21:51 +0200

    sharpening Jml - updating preferences files

---
+++ src/PokerSystem.java
@@ -0,0 +1,19 @@
+/*
+ * PokerMain.java
+ */
+
+
+
+/**
+ * The Class PokerSystem.
+ */
+class PokerSystem {
+
+/**
+ * The main method.
+ * @param args the arguments
+ */
+public static void main(final String[] args) {
+  }
+
+}
